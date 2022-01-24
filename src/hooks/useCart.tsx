bc1998dd69549845daeafae7f4b43fb3e4e44fd0
Lucas Pedro Abreu <lucasp.abreu@gmail.com>
2022-01-24 15:13:16 -0300

    Challenge completed - w/o useRef

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -4,72 +4,135 @@ import { api } from '../services/api';
 import { Product, Stock } from '../types';
 
 interface CartProviderProps {
-  children: ReactNode;
+	children: ReactNode;
 }
 
 interface UpdateProductAmount {
-  productId: number;
-  amount: number;
+	productId: number;
+	amount: number;
+}
+
+interface ProductStock {
+	id: number;
+	amount: number;
 }
 
 interface CartContextData {
-  cart: Product[];
-  addProduct: (productId: number) => Promise<void>;
-  removeProduct: (productId: number) => void;
-  updateProductAmount: ({ productId, amount }: UpdateProductAmount) => void;
+	cart: Product[];
+	addProduct: (productId: number) => Promise<void>;
+	removeProduct: (productId: number) => void;
+	updateProductAmount: ({ productId, amount }: UpdateProductAmount) => void;
 }
 
 const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
-  const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+	const [cart, setCart] = useState<Product[]>(() => {
 
-    return [];
-  });
+		const storagedCart = localStorage.getItem('@RocketShoes:cart')
 
-  const addProduct = async (productId: number) => {
-    try {
-      // TODO
-    } catch {
-      // TODO
-    }
-  };
+		if (storagedCart) {
+			return JSON.parse(storagedCart);
+		}
 
-  const removeProduct = (productId: number) => {
-    try {
-      // TODO
-    } catch {
-      // TODO
-    }
-  };
+		return [];
+	});
 
-  const updateProductAmount = async ({
-    productId,
-    amount,
-  }: UpdateProductAmount) => {
-    try {
-      // TODO
-    } catch {
-      // TODO
-    }
-  };
+	const addProduct = async (productId: number) => {
+		try {
+			const updatedCart = [...cart]
 
-  return (
-    <CartContext.Provider
-      value={{ cart, addProduct, removeProduct, updateProductAmount }}
-    >
-      {children}
-    </CartContext.Provider>
-  );
+			const hasProductOnCart = updatedCart.find(product => productId === product.id)
+
+			const { data: productStock } = await api.get<ProductStock>(`/stock/${productId}`)
+			const amountOnStock = productStock ? productStock.amount : () => toast.error('No reference of product in stock')
+			const amountOnCart = hasProductOnCart ? hasProductOnCart.amount : 0;
+			const desiredAmount = amountOnCart + 1;
+
+			if (desiredAmount > amountOnStock) {
+				toast.error("Quantidade solicitada fora de estoque");
+				return;
+			}
+
+			if (hasProductOnCart) {
+				hasProductOnCart.amount = desiredAmount;
+			} else {
+				const product = await api.get(`/products/${productId}`)
+				const newCartProduct = {
+					...product.data,
+					amount: 1
+				}
+				updatedCart.push(newCartProduct);
+			}
+
+			setCart(updatedCart);
+			localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
+		} catch {
+			toast.error("Erro na adição do produto");
+		}
+	};
+
+	const removeProduct = (productId: number) => {
+		try {
+			const copiedCart = [...cart]
+
+			const hasProductOnCart = copiedCart.find(product => productId === product.id)
+
+			if (hasProductOnCart) {
+				const updatedCart = copiedCart.filter(product => product.id !== productId)
+				localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
+				setCart(updatedCart)
+			} else {
+				throw new Error
+			}
+		} catch {
+			toast.error("Erro na remoção do produto")
+		}
+	};
+
+	const updateProductAmount = async ({
+		productId,
+		amount,
+	}: UpdateProductAmount) => {
+		try {
+			if (amount < 1) return
+
+			const productStock = await api.get(`/stock/${productId}`)
+			const productStockAmount = productStock.data.amount
+
+			if (amount > productStockAmount) {
+				toast.error('Quantidade solicitada fora de estoque')
+				return
+			}
+
+			const updatedCart = [...cart]
+			const hasProductOnCart = updatedCart.find(product => productId === product.id)
+
+			if (hasProductOnCart) {
+				hasProductOnCart.amount = amount
+				localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
+				setCart(updatedCart)
+			} else {
+				throw new Error
+			}
+
+		} catch {
+			toast.error('Erro na alteração de quantidade do produto');
+		}
+	};
+
+	return (
+		<CartContext.Provider
+			value={{ cart, addProduct, removeProduct, updateProductAmount }}
+		>
+			{children}
+		</CartContext.Provider>
+	);
 }
 
 export function useCart(): CartContextData {
-  const context = useContext(CartContext);
+	const context = useContext(CartContext);
 
-  return context;
+	return context;
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,68 +2,70 @@ import styled from 'styled-components';
 import { darken } from 'polished';
 
 export const ProductList = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-gap: 20px;
-  list-style: none;
 
-  li {
-    display: flex;
-    flex-direction: column;
-    background: #fff;
-    border-radius: 4px;
-    padding: 20px;
+	display: grid;
+	grid-template-columns: repeat(3, 1fr);
+	grid-gap: 20px;
+	list-style: none;
 
-    img {
-      align-self: center;
-      max-width: 250px;
-    }
+	li {
 
-    > strong {
-      font-size: 16px;
-      line-height: 20px;
-      color: #333;
-      margin-top: 5px;
-    }
+	display: flex;
+	flex-direction: column;
+	background: #fff;
+	border-radius: 4px;
+	padding: 20px;
 
-    > span {
-      font-size: 21px;
-      font-weight: bold;
-      margin: 5px 0 20px;
-    }
+		img {
+			align-self: center;
+			max-width: 250px;
+		}
 
-    button {
-      background: #7159c1;
-      color: #fff;
-      border: 0;
-      border-radius: 4px;
-      overflow: hidden;
-      margin-top: auto;
+		> strong {
+			font-size: 16px;
+			line-height: 20px;
+			color: #333;
+			margin-top: 5px;
+		}
 
-      display: flex;
-      align-items: center;
-      transition: background 0.2s;
+		> span {
+			font-size: 21px;
+			font-weight: bold;
+			margin: 5px 0 20px;
+		}
 
-      &:hover {
-        background: ${darken(0.06, '#7159c1')};
-      }
+		button {
+			background: #7159c1;
+			color: #fff;
+			border: 0;
+			border-radius: 4px;
+			overflow: hidden;
+			margin-top: auto;
 
-      div {
-        display: flex;
-        align-items: center;
-        padding: 12px;
-        background: rgba(0, 0, 0, 0.1);
+			display: flex;
+			align-items: center;
+			transition: background 0.2s;
 
-        svg {
-          margin-right: 5px;
-        }
-      }
+			&:hover {
+			background: ${darken(0.06, '#7159c1')};
+			}
 
-      span {
-        flex: 1;
-        text-align: center;
-        font-weight: bold;
-      }
-    }
-  }
+			div {
+			display: flex;
+			align-items: center;
+			padding: 12px;
+			background: rgba(0, 0, 0, 0.1);
+
+				svg {
+					margin-right: 5px;
+				}
+			}
+
+			span {
+			flex: 1;
+			text-align: center;
+			font-weight: bold;
+			}
+		}
+	}
 `;
